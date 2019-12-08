ae25694a30e55c61017619a9231f45c248163c05
johan <replokalen@gmail.com>
2019-12-08 23:18:04 +0100

    Approach to day 4.

--- 2019/04.py
+++ 2019/04.py
@@ -1,21 +1,21 @@
-# --- Day 4: Secure Container ---
-#
-# You arrive at the Venus fuel depot only to discover it's protected by a password. The Elves had written the password on a sticky note, but someone threw it out.
-#
-# However, they do remember a few key facts about the password:
-#
-#  - It is a six-digit number.
-#  - The value is within the range given in your puzzle input.
-#  - Two adjacent digits are the same (like 22 in 122345).
-#  - Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
-#
-# Other than the range rule, the following are true:
-#
-#    111111 meets these criteria (double 11, never decreases).
-#    223450 does not meet these criteria (decreasing pair of digits 50).
-#    123789 does not meet these criteria (no double).
-#
-# How many different passwords within the range given in your puzzle input meet these criteria?
-#
-# Your puzzle input is 347312-805915.
+# Approach: go from left to right, evaluate all possible digits recursively.
+# 
+# E.g. given my input 347312-805915.
+# 
+# First digit candidates: 3-8. Loop.
+#   3 => Second digit candidates: 4-9. Loop.
+#       34 => Third digit candidates: 7-9. Loop.
+#           347 => Fourth digit candidates: 7-9. Loop.
+#               3477 => Fifth digit candidates: 7-9. Loop.
+#                   34777 => Sixth digit candidates: 7-9. Add 347777, 347778, 347779 to result.
+#                   34778 => Sixth digit candidates: 8-9. Add 347788, 347789 to result.
+#                   34779 => Sixth digit candidates: 9. Add 347799 to result.
+#               3478 => Fifth digit candidates: 8-9. Loop.
+#                   34788 => Sixth digit candidates: 8-9. Add 347888, 347889 to result.
+#                   34789 => Sixth digit candidates: 9. Add 347899 to result.
+#               3479 => Fifth digit candidates: 9.
+#                   34799 => Sixth digit candidate: 9. Add 347999 to result.
+#           348 => Fourth digit candidates: 8-9. Loop.
+# ....
+# 
 
