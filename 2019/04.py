b6fdf8c56d56bd1979f003bcceee0211406bd03b
johan <replokalen@gmail.com>
2019-12-09 22:39:29 +0100

    Brute force solution for day 4.

--- 2019/04.py
+++ 2019/04.py
@@ -1,21 +1,41 @@
-# Approach: go from left to right, evaluate all possible digits recursively.
-# 
-# E.g. given my input 347312-805915.
-# 
-# First digit candidates: 3-8. Loop.
-#   3 => Second digit candidates: 4-9. Loop.
-#       34 => Third digit candidates: 7-9. Loop.
-#           347 => Fourth digit candidates: 7-9. Loop.
-#               3477 => Fifth digit candidates: 7-9. Loop.
-#                   34777 => Sixth digit candidates: 7-9. Add 347777, 347778, 347779 to result.
-#                   34778 => Sixth digit candidates: 8-9. Add 347788, 347789 to result.
-#                   34779 => Sixth digit candidates: 9. Add 347799 to result.
-#               3478 => Fifth digit candidates: 8-9. Loop.
-#                   34788 => Sixth digit candidates: 8-9. Add 347888, 347889 to result.
-#                   34789 => Sixth digit candidates: 9. Add 347899 to result.
-#               3479 => Fifth digit candidates: 9.
-#                   34799 => Sixth digit candidate: 9. Add 347999 to result.
-#           348 => Fourth digit candidates: 8-9. Loop.
-# ....
-# 
+def is_valid_part_1(number):
+    if number < 347312 or number > 805915:
+        return False
 
+    numstring = str(number)
+ 
+    return numstring[0] <= numstring[1] <= numstring[2] <= numstring[3] <= numstring[4] <= numstring[5] \
+        and (            
+            numstring[0] == numstring[1] or 
+            numstring[1] == numstring[2] or 
+            numstring[2] == numstring[3] or
+            numstring[3] == numstring[4] or 
+            numstring[4] == numstring[5])
+
+
+def is_valid_part_2(number):
+    if number < 347312 or number > 805915:
+        return False
+
+    numstring = str(number)
+
+    return numstring[0] <= numstring[1] <= numstring[2] <= numstring[3] <= numstring[4] <= numstring[5] \
+        and (
+            numstring[0] == numstring[1] != numstring[2] or 
+            numstring[0] != numstring[1] == numstring[2] != numstring[3] or 
+            numstring[1] != numstring[2] == numstring[3] != numstring[4] or
+            numstring[2] != numstring[3] == numstring[4] != numstring[5] or 
+            numstring[3] != numstring[4] == numstring[5])
+
+
+def part_1():
+    return [a for a in range(347312, 805916) if is_valid_part_1(a)]
+
+
+def part_2():
+    return [a for a in range(347312, 805916) if is_valid_part_2(a)]
+
+
+print(len(part_1()))
+print(len(part_2()))
+#print(part_2())
