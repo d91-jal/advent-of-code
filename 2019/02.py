69524f4a257e28f8d3c20f2152adfe4ef76ca93a
johan <replokalen@gmail.com>
2019-12-20 14:11:08 +0100

    Adapted 2, 5 to new IntCode implementation.

--- 2019/02.py
+++ 2019/02.py
@@ -1,4 +1,32 @@
-from Intcode import run_program
+from IntCode import IntCode
+
+
+def gravity_assist(program, expected_result):
+    intcode = IntCode()
+
+    for noun in range(0, 99):
+        for verb in range(0, 99):
+            testprog = program[:]
+            testprog[1], testprog[2] = noun, verb
+            intcode.program = testprog
+            intcode.run_program()
+            result = intcode.program
+
+            if result[0] == expected_result:
+                return (noun * 100) + verb
+
+    return -1, -1
+
+
+def part1(prog):
+    intcode = IntCode()
+    intcode.program = prog[:]
+    intcode.run_program()
+    return intcode.program
+
+
+def part2(prog, expected):
+    return gravity_assist(prog, expected)
 
 
 testprog1 = [1, 0, 0, 0, 99]
@@ -7,29 +35,8 @@ testprog3 = [2, 4, 4, 5, 99, 0]
 testprog4 = [1, 1, 1, 4, 99, 5, 6, 0, 99]
 
 
-def gravity_assist(program, expected_result):
-    for noun in range(0, 99):
-        for verb in range(0, 99):
-            testprog = program[:]
-            testprog[1], testprog[2] = noun, verb
-            result = run_program(testprog)[0]
-
-            if result[0] == expected_result:
-                return (noun * 100) + verb
-
-    return -1, -1
-
-
 program = [int(a) for a in open("02input.txt").read().strip().split(",")]
 program[1] = 12
 program[2] = 2
-print(run_program(program)[0])
-print(gravity_assist(program, 19690720))
-
-
-# print(run_program(testprog1))
-# print(run_program(testprog2))
-# print(run_program(testprog3))
-# print(run_program(testprog4))
-
-
+print((part1(program))[0])
+print(part2(program, 19690720))
--- 2019/05.py
+++ 2019/05.py
@@ -1,6 +1,23 @@
-from Intcode import run_program
+from IntCode import IntCode
+
+
+def run(prog, instr):
+    intcode = IntCode()
+    intcode.program = prog[:]
+    intcode.input_buffer = [instr]
+    intcode.run_program()
+    return intcode.output_buffer[0]
+
+
+def part1(prog):
+    return run(prog, 1)
+
+
+def part2(prog):
+    return run(prog, 5)
 
 
 program = [int(a) for a in open("05input.txt").read().strip().split(",")]
-run_program(program, [1])
-run_program(program, [5])
+print(part1(program))
+print(part2(program))
+
