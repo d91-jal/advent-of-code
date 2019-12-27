6f63cce4168627f6b7ea5ab074d81f30a123d5e7
johan <replokalen@gmail.com>
2019-12-27 15:08:12 +0100

    Day 9 done.

--- aoc2019/intcode.py
+++ aoc2019/intcode.py
@@ -24,41 +24,43 @@ class IntCode:
         self.relative_base = 0
         self.memory_size = memory_size
 
-    def get_param(self, mode, program, index):
+    def get_address(self, mode, index):
         if mode == "0":     # Position mode
-            return program[program[index]]
+            return self.program[index]
         elif mode == "1":   # Immediate mode
-            return program[index]
+            return index
         elif mode == "2":   # Relative mode
-            return program[program[index] + self.relative_base]
+            return self.program[index] + self.relative_base
+
+    def get_param(self, mode, index):
+        return self.program[self.get_address(mode, index)]
+
+    def set_param(self, mode, index, value):
+        self.program[self.get_address(mode, index)] = value
 
     def run_program(self):
-        result = self.program[:]
         self.running = True
-        # self.output_buffer = []
 
-        while self.p < len(result):
-            if (self.p + 1 > len(result)) or (result[self.p] == 99):
+        while self.p < len(self.program):
+            if (self.p + 1 > len(self.program)) or (self.program[self.p] == 99):
                 self.p = 0
                 self.running = False
                 break
 
-            opcode, mode1, mode2, mode3 = parse_instruction(str(result[self.p]))
+            opcode, mode1, mode2, mode3 = parse_instruction(str(self.program[self.p]))
 
             if opcode == "01":      # Addition
-                result[result[self.p + 3]] = \
-                    self.get_param(mode1, result, self.p + 1) + self.get_param(mode2, result, self.p + 2)
+                self.set_param(mode3, self.p + 3, self.get_param(mode1, self.p + 1) + self.get_param(mode2, self.p + 2))
                 self.p += 4
             elif opcode == "02":    # Multiplication
-                result[result[self.p + 3]] = \
-                    self.get_param(mode1, result, self.p + 1) * self.get_param(mode2, result, self.p + 2)
+                self.set_param(mode3, self.p + 3, self.get_param(mode1, self.p + 1) * self.get_param(mode2, self.p + 2))
                 self.p += 4
             elif opcode == "03":    # Read input
                 input_val = self.input_buffer.pop(0) if len(self.input_buffer) > 0 else read_input()
-                result[result[self.p + 1]] = input_val
+                self.set_param(mode1, self.p + 1, input_val)
                 self.p += 2
             elif opcode == "04":    # Print output
-                out_val = self.get_param(mode1, result, self.p + 1)
+                out_val = self.get_param(mode1, self.p + 1)
                 self.output_buffer.append(out_val)
 
                 if self.stdout:
@@ -67,30 +69,29 @@ class IntCode:
                 self.p += 2
                 break
             elif opcode == "05":    # Jump if true
-                if self.get_param(mode1, result, self.p + 1) != 0:
-                    self.p = self.get_param(mode2, result, self.p + 2)
+                if self.get_param(mode1, self.p + 1) != 0:
+                    self.p = self.get_param(mode2, self.p + 2)
                 else:
                     self.p += 3
             elif opcode == "06":    # Jump if false
-                if self.get_param(mode1, result, self.p + 1) == 0:
-                    self.p = self.get_param(mode2, result, self.p + 2)
+                if self.get_param(mode1, self.p + 1) == 0:
+                    self.p = self.get_param(mode2, self.p + 2)
                 else:
                     self.p += 3
             elif opcode == "07":    # Less than
-                result[result[self.p + 3]] = \
-                    1 if self.get_param(mode1, result, self.p + 1) < self.get_param(mode2, result, self.p + 2) else 0
+                new_val = 1 if self.get_param(mode1, self.p + 1) < self.get_param(mode2, self.p + 2) else 0
+                self.set_param(mode3, self.p + 3, new_val)
                 self.p += 4
             elif opcode == "08":    # Equals
-                result[result[self.p + 3]] = \
-                    1 if self.get_param(mode1, result, self.p + 1) == self.get_param(mode2, result, self.p + 2) else 0
+                new_val = 1 if self.get_param(mode1, self.p + 1) == self.get_param(mode2, self.p + 2) else 0
+                self.set_param(mode3, self.p + 3, new_val)
                 self.p += 4
             elif opcode == "09":    # Relative base
-                self.relative_base += self.get_param(mode1, result, self.p + 1)
+                self.relative_base += self.get_param(mode1, self.p + 1)
                 self.p += 2
             else:                   # Undefined = NOP
                 self.p += 1
 
-        self.program = result[:]
         return
 
 
