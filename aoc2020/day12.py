713f1c9256a86993a98d5cb7d7cf50a57ea31d61
johan <replokalen@gmail.com>
2021-01-08 16:03:09 +0100

    Day 12 refactoring.

--- aoc2020/day12.py
+++ aoc2020/day12.py
@@ -9,36 +9,45 @@ def get_input():
     return my_input
 
 
+# pos = [north_coord, east_coord, heading]
+# change = command from input file
+def move(pos, change):
+    cmd = change[0]
+    arg = int(change[1:])
+
+    if cmd == 'N':
+        pos[1] += arg
+    elif cmd == 'S':
+        pos[1] -= arg
+    elif cmd == 'E':
+        pos[0] += arg
+    elif cmd == 'W':
+        pos[0] -= arg
+    elif cmd == 'L':
+        pos[2] -= math.pi * (arg / 180.0)
+    elif cmd == 'R':
+        pos[2] += math.pi * (arg / 180.0)
+    elif cmd == 'F':
+        pos[0] += arg * math.cos(pos[2] - (math.pi / 2.0))     # Subtract pi/2 as E=0 in this coordinate system.
+        pos[1] += arg * math.sin(pos[2] - (math.pi / 2.0))     # Subtract pi/2 as E=0 in this coordinate system.
+
+    return pos
+
+
 def part_1():
     my_input = get_input()
-    pos = [0, 0]
-    heading = 0   # E=0, S=pi/2, W=pi, N=3pi/2
-    
-    for instr in my_input:
-        cmd = instr[0]
-        arg = int(instr[1:])
+    pos = [0, 0, 0]  # pos = [north_coord, east_coord, heading]
 
-        if cmd == 'N':
-            pos[1] += arg
-        elif cmd == 'S':
-            pos[1] -= arg
-        elif cmd == 'E':
-            pos[0] += arg
-        elif cmd == 'W':
-            pos[0] -= arg
-        elif cmd == 'L':
-            heading -= math.pi * (arg / 180.0)
-        elif cmd == 'R':
-            heading += math.pi * (arg / 180.0)
-        elif cmd == 'F':
-            pos[0] += arg * math.cos(heading - (math.pi / 2.0))     # Subtract pi/2 as E=0 in this coordinate system.
-            pos[1] += arg * math.sin(heading - (math.pi / 2.0))     # Subtract pi/2 as E=0 in this coordinate system.
+    for instr in my_input:
+        pos = move(pos, instr)
 
     return int(abs(pos[0]) + abs(pos[1]))
 
 
 def part_2():
-    return 2
+    my_input = get_input()
+    pos = [0, 0, 0]
+    waypoint = [1, 10, 0]
     
 
 def main():
