350b5bf322f99da4ed2e359aaf74e67c7cbfa577
Johan Almén <replokalen@gmail.com>
2023-12-13 08:48:57 +0100

    Day 3 skeleton

---
+++ aoc2023/day03.py
@@ -0,0 +1,19 @@
+def part_1(my_input):
+    return
+
+
+def part_2(my_input):
+    return
+
+
+def main():
+    # Read input into an array.
+    input_file = open("resources/input03.txt")
+    my_input = input_file.read().strip().split("\n")
+    input_file.close()
+    print(part_1(my_input))
+    print(part_2(my_input))
+
+
+if __name__ == "__main__":
+    main()
