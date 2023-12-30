724a0a24d9606512bd40b1c8fb624860e9cfc510
d91-jal <replokalen@gmail.com>
2023-12-30 13:35:45 +0100

    Day 16

--- aoc2020/day16.py
+++ aoc2020/day16.py
@@ -1,4 +1,49 @@
+def get_notes():
+    input_file = open("resources/input16_1.txt")
+    notes = [a for a in input_file.read().strip().split('\n')]
+    input_file.close()
+    return notes
+
+
+def get_ticket():
+    input_file = open("resources/input16_2.txt")
+    notes = [a for a in input_file.read().strip().split('\n')]
+    input_file.close()
+    return notes
+
+
+def get_nearby_tickets():
+    input_file = open("resources/input16_3.txt")
+    notes = [a for a in input_file.read().strip().split('\n')]
+    input_file.close()
+    return notes
+
+
+def get_valid_numbers():
+    notes = get_notes()
+    intervals = []
+    match_list = []
+
+    for note in notes:
+        parts = note.split(": ")
+        for part in parts[1].split(" or "):
+            intervals.append(part)
+
+    for x in intervals:
+        interval = x.split('-')
+        match_list.append((int(interval[0]), int(interval[1])))
+
+    return match_list
+
+
 def part_1():
+    valid = get_valid_numbers()
+    
+    
+    print(match_list)
+
+
+
     return 1
 
 
