0ed6c5c4ecd5af1113d53448ec08893237a50d47
Johan Almén <replokalen@gmail.com>
2024-12-12 12:43:45 +0100

    2024 day 3

---
+++ aoc2024/day03.py
@@ -0,0 +1,44 @@
+import re
+
+
+def part_1(my_input):
+    expr = r'mul\((\d+),(\d+)\)'
+    result = 0
+
+    for line in my_input:
+        for args in re.findall(expr, line):
+            result += (int(args[0]) * int(args[1]))
+
+    return result
+
+
+def part_2(my_input):
+    do = r'do\(\)'
+    dont = r'don\'t\(\)'
+    expr = r'mul\((\d+),(\d+)\)'
+    result = 0 
+    merged_input = ''.join(my_input)
+    
+    # Partition on do() instructions.
+    for start in re.split(do, merged_input):
+        # Find cutoff for each do.
+        remaining = re.split(dont, start)[0]
+        # Parse the cleaned instructions.
+        for args in re.findall(expr, remaining):
+            result += (int(args[0]) * int(args[1]))
+            
+    return result
+
+
+def main():
+    # Read input into a list.
+    input_file = open("aoc2024/resources/input03.txt")
+    my_input = input_file.readlines()
+    input_file.close()
+
+    print(part_1(my_input))
+    print(part_2(my_input))
+
+
+if __name__ == "__main__":
+    main()
