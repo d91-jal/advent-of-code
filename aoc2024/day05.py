bef58d9503419f144f38e4604e5490b8902fe015
Johan Almén <replokalen@gmail.com>
2024-12-16 16:06:05 +0100

    2024 day 5 rewrite for part 2

--- aoc2024/day05.py
+++ aoc2024/day05.py
@@ -1,60 +1,85 @@
-def validate(pre_pages, post_pages, dep_pages):
+def validate_page(pre_pages, post_pages, dep_pages):
     for page in pre_pages:
         if page not in dep_pages:
-            return False
+            return page
         
     for page in post_pages:
         if page in dep_pages:
-            return False
+            return page
         
-    return True
+    return -1
 
 
-def part_1(my_input):
-    result = 0
-    deps = {}
+def validate_batch(pages, dep_pages):
+    validated = True
+    fails = []
+    i = 0
+
+    while i < len(pages):
+        page = pages[i]
+
+        if page in dep_pages:
+            if validate_page(pages[:i], pages[i:], dep_pages[page]) >= 0:
+                fails += [i]
+
+        i += 1
+
+    return fails
+
+
+def build_dependency_map(my_input):
+    result = {}
 
     # Add all the dependencies for each page number in a list.
-    for pair in my_input[0]:
+    for pair in my_input:
         a, b = map(int, pair.split("|"))
-        if b in deps:
-            deps[b] += [a]
+        if b in result:
+            result[b] += [a]
         else:
-            deps[b] = [a]    
-
-    # For each page in a batch, check that all dependencies are in fulfilled.
-    for batch in my_input[1]:
-        page_list = [int(a) for a in batch.split(",")]
-        validated = True
-        i = 0 
-
-        while validated and i < len(page_list):
-            page_num = page_list[i]
-            
-            if page_num in deps:                    
-                if not validate(page_list[:i], page_list[i:], deps[page_num]):
-                    validated = False
-
-            i += 1
-
-        # If validated, or if the page has no dependencies:
-        if validated: 
-            result += page_list[int((len(page_list) - 1) / 2)]
+            result[b] = [a]    
 
     return result
 
 
+def part_1(my_input):
+    result = 0
+    deps = build_dependency_map(my_input[0])
+
+    # For each page in a batch, check that all dependencies are in fulfilled.
+    for batch in my_input[1]:
+        page_list = [int(a) for a in batch.split(",")]
+        fails = validate_batch(page_list, deps)
+
+        # If validated, or if the page has no dependencies:
+        if len(fails) == 0:
+            result += page_list[(len(page_list) - 1) // 2]
+
+    return result
 
 
 def part_2(my_input):
     result = 0
+    deps = build_dependency_map(my_input[0])
+
+    # For each page in a batch, check that all dependencies are in fulfilled.
+    for batch in my_input[1]:
+        page_list = [int(a) for a in batch.split(",")]
+        fails = validate_batch(page_list, deps)
+
+        # If not validated, re-sort by number of dependencies and re-validate.
+        if len(fails) > 0:
+            print(page_list, fails)
+            #sorted_page_list = sorted(page_list, key=lambda x: len(deps.get(x, [])))
+
+            #if validate_batch(sorted_page_list, deps):
+            #    result += sorted_page_list[(len(sorted_page_list) - 1) // 2]
 
     return result
 
 
 def main():
     # Read input into a list.
-    input_file = open("aoc2024/resources/input05.txt")
+    input_file = open("aoc2024/resources/test05.txt")
     my_input = [a.split() for a in input_file.read().strip().split('\n\n')]
     input_file.close()
     print(part_1(my_input))
