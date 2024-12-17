d607b7635eb6f69ed3d74d7bb771a328c125296e
d91-jal <replokalen@gmail.com>
2024-12-17 23:19:24 +0100

    Simple solution for day 6 part 1

--- aoc2024/day06.py
+++ aoc2024/day06.py
@@ -8,43 +8,57 @@ directions = {
 }
 
 def turn_right(dir):
-    return dir
+    if dir == (0, 1):
+        return (1, 0)
+    elif dir == (1, 0):
+        return (0, -1)
+    elif dir == (0, -1):
+        return (-1, 0)
+    elif dir == (-1, 0):
+        return (0, 1)
+    else:
+        return (-1, 0)
+    
 
 def part_1(my_input):
     guard = r'\^|v|<|>'
-
-
     height = len(my_input)
     width = len(my_input[0])
     result = 1
     done = False
     y = 0
-
-    # Find the starting position and direction.
+    charmap = []
+    
+    # Find the starting position and direction, and convert the map into 
+    # lists of chars for easier manipulation.
     for line in my_input:
         match = re.search(guard, line)
+        charmap.append(list(line))
                 
         if match != None:
             dir = directions[match.group(0)]
             pos = (y, (match.span(0)[0]))
+            charmap[pos[0]][pos[1]] = 'X'
             print("Initial state:", pos, dir)
         else:
             y += 1
 
+    
     # Traverse the map, updating the count of new locations visited.
     while not done:
-        next = pos + dir
+        next = tuple(map(lambda x, y: x + y, pos, dir))
         
-        if next[0] < 0 or next[1] < 0 or next[0] > height or next[1] > width:
+        if next[0] < 0 or next[1] < 0 or next[0] >= height or next[1] >= width:
+            done = True
             break
-        elif my_input[next[0]][next[1]] == '.':
+        elif charmap[next[0]][next[1]] == '.':
             result += 1
-            my_input[pos[0]][pos[1]] = 'X'
+            pos = next
+            charmap[pos[0]][pos[1]] = 'X'
+        elif charmap[next[0]][next[1]] == 'X':
             pos = next
         elif my_input[next[0]][next[1]] == '#':
             dir = turn_right(dir)
-
-        done = True
 
     return result
 
@@ -56,7 +70,7 @@ def part_2(my_input):
 
 def main():
     # Read input into a list.
-    input_file = open("aoc2024/resources/test06.txt")
+    input_file = open("aoc2024/resources/input06.txt")
     my_input = input_file.read().strip().split("\n")
     input_file.close()
 
