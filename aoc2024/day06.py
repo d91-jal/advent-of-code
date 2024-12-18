cab559fafa91969b9fee2c789588d4e86cf47010
Johan Almén <replokalen@gmail.com>
2024-12-18 16:03:29 +0100

    2024 day 6 part 2 incomplete

--- aoc2024/day06.py
+++ aoc2024/day06.py
@@ -1,11 +1,23 @@
-import re
 
-directions = { 
-        '>' : (0, 1),  # Right
-        '<' : (0, -1), # Left
-        'v' : (1, 0),  # Down
-        '^' : (-1, 0)   # Up
-}
+def build_map(my_input):
+    charmap = []
+    y = 0
+
+    # Find the starting position and direction, and convert the map into 
+    # lists of chars for easier manipulation.
+    for line in my_input:
+        x = line.rfind(r'^')
+        charmap.append(list(line))
+                
+        if x >= 0:
+            dir = (-1, 0)
+            pos = (y, x)
+            charmap[pos[0]][pos[1]] = 'X'
+        else:
+            y += 1
+
+    return charmap, pos, dir
+
 
 def turn_right(dir):
     if dir == (0, 1):
@@ -21,29 +33,12 @@ def turn_right(dir):
     
 
 def part_1(my_input):
-    guard = r'\^|v|<|>'
     height = len(my_input)
     width = len(my_input[0])
     result = 1
     done = False
-    y = 0
-    charmap = []
-    
-    # Find the starting position and direction, and convert the map into 
-    # lists of chars for easier manipulation.
-    for line in my_input:
-        match = re.search(guard, line)
-        charmap.append(list(line))
-                
-        if match != None:
-            dir = directions[match.group(0)]
-            pos = (y, (match.span(0)[0]))
-            charmap[pos[0]][pos[1]] = 'X'
-            print("Initial state:", pos, dir)
-        else:
-            y += 1
-
-    
+    charmap, pos, dir = build_map(my_input)
+        
     # Traverse the map, updating the count of new locations visited.
     while not done:
         next = tuple(map(lambda x, y: x + y, pos, dir))
@@ -65,12 +60,34 @@ def part_1(my_input):
 
 def part_2(my_input):
     result = 0
+    height = len(my_input)
+    width = len(my_input[0])
+    done = False
+    charmap, pos, dir = build_map(my_input)
+        
+    # Naive assumption: traverse the map twice, the second time adding one
+    # to the result each time we cross our own path.
+    while not done:
+        next = tuple(map(lambda x, y: x + y, pos, dir))
+        
+        if next[0] < 0 or next[1] < 0 or next[0] >= height or next[1] >= width:
+            done = True
+            break
+        elif charmap[next[0]][next[1]] == '.':
+            pos = next
+            charmap[pos[0]][pos[1]] = 'X'
+        elif charmap[next[0]][next[1]] == 'X':
+            result += 1
+            pos = next
+        elif my_input[next[0]][next[1]] == '#':
+            dir = turn_right(dir)
+
     return result
 
 
 def main():
     # Read input into a list.
-    input_file = open("aoc2024/resources/input06.txt")
+    input_file = open("aoc2024/resources/test06.txt")
     my_input = input_file.read().strip().split("\n")
     input_file.close()
 
